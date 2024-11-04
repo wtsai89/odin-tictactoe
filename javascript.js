d5e004bfe925f6b4d05beb00c6b7a3587feb545b
William Tsai <wtsai_us@yahoo.com>
2024-11-03 22:44:59 -0800

    add graphical components

--- javascript.js
+++ javascript.js
@@ -25,7 +25,15 @@ function Gameboard() {
         console.log(printout);
     }
 
-    return {getBoard, placeToken, printBoard};
+    const resetBoard = () => {
+        for(let i = 0; i < rows; i++) {
+            for(let j = 0; j < cols; j++) {
+                board[i][j].addToken(0);
+            }
+        }
+    }
+
+    return {getBoard, placeToken, printBoard, resetBoard};
 }
 
 function Cell() {
@@ -61,6 +69,13 @@ function GameController(
     ];
 
     let activePlayer = players[0];
+
+    const updatePlayerNames = () => {
+        const playerOne = document.querySelector("#player1");
+        const playerTwo = document.querySelector("#player2");
+        players[0].name = playerOne.value || "Player One";
+        players[1].name = playerTwo.value || "Player Two";
+    }
 
     const switchPlayerTurn = () => {
         activePlayer = activePlayer === players[0] ? players[1] : players[0];
@@ -115,9 +130,84 @@ function GameController(
         return 0;
     };
 
+    const reset = () => {
+        board.resetBoard();
+        activePlayer = players[0];
+    }
+
     printNewRound();
 
-    return { getActivePlayer, playRound };
+    return { getActivePlayer, playRound, updatePlayerNames, getBoard: board.getBoard, reset};
 }
 
-const game = GameController();
+function ScreenController() {
+    const game = GameController();
+    const promptDiv = document.querySelector(".playerPrompt");
+    const boardDiv = document.querySelector(".tttgrid");
+    let active = false;
+
+    const updateScreen = () => {
+        const board = game.getBoard();
+        const printout = board.map((row) => row.map((cell) => cell.getValue()));
+        const activePlayer = game.getActivePlayer();
+
+        promptDiv.textContent = `${activePlayer.name}'s turn`;
+
+        for (const div of boardDiv.children) {
+            for (const button of div.children) {
+                const buttonRow = button.dataset.row;
+                const buttonCol = button.dataset.col;
+                const cellValue = printout[buttonRow][buttonCol];
+                if (cellValue === 0)
+                    button.style.background = "none";
+                else if (cellValue === 1)
+                    button.style.backgroundImage = "url(./x.svg)";
+                else
+                    button.style.backgroundImage = "url(./circle-outline.svg)";
+                button.style.backgroundSize = "cover";
+            }
+        }
+    }
+
+    function clickHandlerBoard(e) {
+        const selectedRow = e.target.dataset.row;
+        const selectedCol = e.target.dataset.col;
+
+        if (!selectedRow || !active) return;
+
+        result = game.playRound(selectedRow, selectedCol);
+        updateScreen();
+
+        if (result === 1) {
+            promptDiv.textContent = `${game.getActivePlayer().name} Wins!`;
+            active = false;
+        }
+
+        //console.log(`Row ${selectedRow} Col ${selectedCol}`);
+    }
+    boardDiv.addEventListener("click", clickHandlerBoard);
+
+    function toggleActive() {
+        game.reset();
+        game.updatePlayerNames();
+        updateScreen();
+        active = true;
+        console.log(active);
+    }
+
+    return { toggleActive } ;
+}
+
+const game = ScreenController();
+
+const dialog = document.querySelector("dialog");
+const newGameButton = document.querySelector(".newGame");
+newGameButton.addEventListener("click", () => {
+    dialog.showModal();
+})
+const submitButton = document.querySelector("form button");
+submitButton.addEventListener("click", (e) => {
+    game.toggleActive();
+    dialog.close();
+    e.preventDefault();
+})
